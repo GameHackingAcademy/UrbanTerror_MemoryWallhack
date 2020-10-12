43d822efc706cd36d9d6bba81e81894791933f70
attilathedud <attilathedud@gmail.com>
2020-10-12 11:24:24 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
+/*
+A wallhack for Urban Terror 4.3.4 that reveals entities through walls by disabling depth-testing.
+
+This is done by modifying each entity's render flag, which is responsible for determining how the entity should be rendered. By setting this value to the in-game value for disabled depth testing (0xD), entities will be drawn regardless of if it should visible. The code hooked is a mov instruction which occurs after ebx is loaded with a valid entity structure.
+
+This must be injected into the Urban Terror process to work. One way to do this is to use a DLL injector. Another way is to enable AppInit_DLLs in the registry.
+
+The offsets and method to discover them are discussed in the article at: https://gamehacking.academy/lesson/22
+*/
 #include <Windows.h>
 
 DWORD ret_address = 0x0052D303;
 
+// Our codecave that program execution will jump to. The declspec naked attribute tells the compiler to not add any function
+// headers around the assembled code
 __declspec(naked) void codecave() {
-
+	// Asm blocks allow you to write pure assembly
+	// In this case, we use it to save all the registers
+	// Then set the entity's render value at [ebx+4] to disabled depth testing (0xD)
+	// Then we restore the registers, recreate the original instruction, and jump back to the program code
 	__asm {
 		pushad
 		mov dword ptr ds:[ebx+4], 0xD
@@ -14,6 +28,10 @@ __declspec(naked) void codecave() {
 	}
 }
 
+// When our DLL is attached, unprotect the memory at the code we wish to write at
+// Then set the first opcode to E9, or jump
+// Caculate the location using the formula: new_location - original_location+5
+// Finally, since the original instructions totalled 6 bytes, NOP out the last remaining byte
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
 	DWORD old_protect;
 	unsigned char* hook_location = (unsigned char*)0x0052D2FD;
